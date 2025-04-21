16f3d3ec38a0db65c658a85f5a6acccdff5dae0b
Kishan <novasuper11@proton.me>
2025-04-21 08:45:08 +0000

    Initial commit and get CLI arguments

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let pattern = std::env::args().nth(1).expect("No pattern given");
+    let path = std::env::args().nth(2).expect("No path given");
+
+    println!("Pattern: {:?}, Path: {:?}", pattern, path);
+}
