ea1706f0812767f800322c951efcf6c21721bb20
Kishan <novasuper11@proton.me>
2025-04-21 09:21:14 +0000

    Use clap to parse CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-struct Cli {
+use clap::Parser;
+
+/// Search for a pattern in file and display the lines that contain it.
+#[derive(Parser)]
+struct Args {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
-fn main() {
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("No path given");
 
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+fn main() {
+    let args = Args::parse();
 
     println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
 }
