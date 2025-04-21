546330362ff19129a724484b346a8e497f1c88b8
Kishan <novasuper11@proton.me>
2025-04-21 08:52:06 +0000

    Add struct to represent args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 fn main() {
     let pattern = std::env::args().nth(1).expect("No pattern given");
     let path = std::env::args().nth(2).expect("No path given");
 
-    println!("Pattern: {:?}, Path: {:?}", pattern, path);
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
 }
